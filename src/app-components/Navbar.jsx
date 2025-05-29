6d9aee96cb31312f81a63077b7dc41b56bf21b69
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2025-05-29 23:47:54 +0530

    working-on-navbar

--- src/app-components/Navbar.jsx
+++ src/app-components/Navbar.jsx
@@ -17,7 +17,7 @@ import { Link } from "react-router";
 
 function Navbar() {
   return (
-    <div className="flex p-4 items-center justify-between">
+    <div className="flex fixed w-full top-0 left-0  p-4 items-center bg-red-700 justify-between">
       <SidebarTrigger />
       <div>
         {/* <Link href="/">Dashboard</Link> */}
